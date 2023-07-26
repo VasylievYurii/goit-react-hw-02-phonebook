4242de5d12f30bc61c89503d97bf84ccb3b9d622
VasylievYurii <uraplanetianin@gmail.com>
2023-07-26 13:48:58 +0300

    added localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -22,6 +22,20 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = data => {
     const normalizedNameFilter = data.name.toLowerCase();
     const isFoundName = !this.state.contacts.find(contact =>
